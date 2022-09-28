07597d649f4832ce3b6945fc33c4bbe77c78c362
Fred Tang (唐暐凡) <fredtang@ns-guard.com>
2022-09-28 18:45:55 +0800

    update

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,2 @@
 def hello_world():
-    print(hello_world)
-    
-if __name__ == "__main__":
-    hello_world()
+    print("hello_world")
--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,11 +1,10 @@
+import sys
 import unittest
 
-from .. import hello_world
+sys.path.append('..')
+from hello_world import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     
     def test_hello_world(self):
         hello_world()
-
-if __name__ == '__main__':
-    unittest.main()
